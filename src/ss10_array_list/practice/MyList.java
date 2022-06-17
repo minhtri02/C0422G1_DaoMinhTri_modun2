3ce00c227653ae061781e2b5f65e400f7ba5341d
unknown <you@example.com>
2022-06-17 20:50:45 +0700

    17-6-22

--- src/ss10_array_list/practice/MyList.java
+++ src/ss10_array_list/practice/MyList.java
@@ -1,8 +1,38 @@
 package ss10_array_list.practice;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MyList<E> {
+//    public static void main(String[] args) {
+//        ArrayList<String> hayqya= new ArrayList<String>();
+//        hayqya.add("tri");
+//        hayqya.add("thanh");
+//        hayqya.add("tam");
+//        hayqya.add("kạldjak");
+//hayqya.add(2,"hanh");
+//hayqya.add("kjkad");
+//hayqya.add("kjkad");
+//hayqya.add("kjkad");
+//hayqya.add("kjkad");
+//hayqya.add("kjkad");
+//hayqya.add("kjkad");
+//        System.out.println(hayqya.size());
+//        System.out.println(hayqya.ensureCapacity());
+////        for (String s : hayqya         ) {
+////            System.out.println(s);
+////        }
+////        System.out.println(hayqya);
+////        System.out.println(hayqya.contains("tri"));
+//        System.out.println(hayqya);
+////        String bc =Arrays.toString(hayqya.toArray());
+//
+//        System.out.println(hayqya.lastIndexOf("tri"));
+//    }
+
+
+
+
     private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
     private Object elements[];
@@ -23,9 +53,17 @@ public class MyList<E> {
         elements[size++] = e;
     }
 
+    @Override
+    public String toString() {
+        return "MyList{" +
+                "size=" + size +
+                ", elements=" + Arrays.toString(elements) +
+                '}';
+    }
+
     public E get(int i) {
         if (i >= size || i < 0) {
-            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i);
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i );
         }
         return (E) elements[i];
     }
@@ -37,16 +75,23 @@ public class MyList<E> {
         listInteger.add(3);
         listInteger.add(3);
         listInteger.add(4);
+        listInteger.add(5);
+        listInteger.add(6);
+        listInteger.add(7);
 
 //        System.out.println(Arrays.toString(listInteger));
         System.out.println("element 4: "+listInteger.get(4));
         System.out.println("element 1: "+listInteger.get(1));
         System.out.println("element 2: "+listInteger.get(2));
+//        System.out.println("element 2: "+listInteger.get(10));
+//        System.out.println("element 2: "+listInteger.get(10));
 
-//        listInteger.get(6);
-//        System.out.println("element 6: "+listInteger.get(6));
-
-        listInteger.get(-1);
-        System.out.println("element -1: " + listInteger.get(-1));
+////        listInteger.get(6);
+////        System.out.println("element 6: "+listInteger.get(6));
+//        System.out.println(listInteger);
+////        listInteger.get(-1);
+////        System.out.println("element -1: " + listInteger.get(-1));
+//       listInteger.ensureCapa();
+        System.out.println(listInteger);
     }
 }
---
+++ src/ss10_array_list/practice/exercise/MyList.java
@@ -0,0 +1,112 @@
+package ss10_array_list.practice.exercise;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MyList<E> {
+    public static void main(String[] args) {
+        MyList<Integer> myList = new MyList<>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+
+//        myList.remove(2);
+        myList.add(2,3);
+
+        System.out.println(Arrays.toString(myList.elements));
+
+//        System.out.println(myList.toArray());
+    }
+    private int size ;
+    static final int DEFAULT_CAPACITY =10;
+    private Object[] elements;
+    private int index;
+    private E element;
+
+    public MyList() {
+        this.elements=new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyList(int infinityCapacity){
+        if (infinityCapacity > 0) {
+            this.elements = new Object[infinityCapacity];
+        } else if (infinityCapacity == 0) {
+
+        } else {
+            System.out.println("Mảng không thể là số âm!");
+        }
+    }
+
+    public void add(int index, E element) {
+        checkIndex(index);
+        if (size == elements.length){
+            ensureCapa();
+        }
+        for (int i = size; i >= index; i--) {
+            elements[i+1] = elements[i];
+        }
+        elements[index] = element;
+        size++;
+    }
+    public void add(E element){
+        if (size == elements.length) {
+            ensureCapa();
+        }
+        elements[size++] = element;
+    }
+
+    private void ensureCapa() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException
+                    ("index " + index + " out of bounds");
+    }
+
+    public E remove(int index) {
+        checkIndex(index);
+
+        E e = (E) elements[index];
+
+        // Shift data to the left
+        for (int j = index; j < size - 1; j++)
+            elements[j] = elements[j + 1];
+
+        elements[size - 1] = null; // This element is now null
+
+        // Decrement size
+        size--;
+        return e;
+    }
+
+    public int indexOf(Object o) {
+        if (o == null) {
+            for (int i = 0; i < size; i++)
+                if (elements[i]==null)
+                    return i;
+        } else {
+            for (int i = 0; i < size; i++)
+                if (o.equals(elements[i]))
+                    return i;
+        }
+        return -1;
+    }
+
+    public Object[] toArray() {
+        return Arrays.copyOf(elements, size);
+    }
+
+}
